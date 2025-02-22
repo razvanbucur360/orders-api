4212104de2f21ea0349fafd7999e1f450b027830
rbucur-plenty <razvan.bucur@plentyone.com>
2025-02-22 16:41:43 +0200

    add redis client

--- application/app.go
+++ application/app.go
@@ -4,16 +4,22 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb *redis.Client
 }
 
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
+		rdb: redis.NewClient(&redis.Options{}),
 	}
+
 
 	return app
 }
@@ -24,11 +30,38 @@ func (a *App) Start(ctx context.Context) error {
 		Handler: a.router,
 	}
 
-	err := server.ListenAndServe()
-
+	err := a.rdb.Ping(ctx).Err()
 	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
+		return fmt.Errorf("Could not connect to redis: %w", err)
 	}
 
-	return nil
+	defer func() {
+		if err := a.rdb.Close()
+		err != nil {
+			fmt.Println("Failed to close reids", err)
+		}
+	}()
+
+	fmt.Println("Connected to redis")
+
+	ch := make(chan error, 1)
+
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+		close(ch)
+	}()	
+
+	select {
+	case err = <- ch:
+		return err
+	case <- ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel() 
+		return server.Shutdown(timeout)
+	}
+
+	
 }
